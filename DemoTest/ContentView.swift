10d03bfae0679c58ac6ccc92851d8d6001396452
Dahyanna Hernandez <dahyannahernandez@gmail.com>
2022-05-25 16:21:01 -0400

    Initial Commit

---
+++ DemoTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoTest
+//
+//  Created by Dahyanna on 25-05-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoTest/DemoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTestApp.swift
+//  DemoTest
+//
+//  Created by Dahyanna on 25-05-22.
+//
+
+import SwiftUI
+
+@main
+struct DemoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
